458bb0a9fb40cc18955ad316bccd693d402cd130
Shichen Fan <martinfanshichen@hotmail.com>
2018-11-28 09:18:44 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -256,7 +256,7 @@ var variableMap = {
   },
   */
   "Mobility_._Congestion":{
-    "name": "Mobility & Congestion",
+    "name": "Mobility and Congestion",
     "description": "Travel Time Index on project link",
     "column_chart": false,
     "format": "decimal"
