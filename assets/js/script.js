aa36b9fc4b8af8aab6136084dd99972156649a96
Shichen Fan <martinfanshichen@hotmail.com>
2018-12-07 11:40:39 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1342,7 +1342,7 @@ function getColorScale(row){
 		
 		else  if (colorVariable === 'goods_move'){
 			var colorDomain = [0,100]; 
-			breaks = [0,50,100]
+			breaks = [0,10,20,30,40,50,60,70,80,90,100]
 			jenks[colorVariable] = d3.scale.quantile()
 		    	.domain(breaks)
 		    	.range(colorbrewer.RdPu.mod7)
