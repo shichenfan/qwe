76a9d381784b4b5ed04a52549cd73152d280c97d
Shichen Fan <martinfanshichen@hotmail.com>
2018-12-11 16:33:13 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1347,7 +1347,7 @@ function getColorScale(row){
 		 }
 		else  if (colorVariable === 'reliability'){
 			var colorDomain = [+_.min(filteredRows,colorVariable)[colorVariable],100];
-			breaks = ss.jenks(csvRows.map(function(d) { return +d[colorVariable]; }), 7)
+			breaks = [0, undefined, 100]
 			jenks[colorVariable] = d3.scale.quantile()
 		   	 .domain(breaks)
 		    	.range(colorbrewer.RdPu.mod7)
@@ -1355,7 +1355,7 @@ function getColorScale(row){
 		}
 		else  if (colorVariable === 'connect'){
 			var colorDomain = [+_.min(filteredRows,colorVariable)[colorVariable],+_.max(filteredRows,colorVariable)[colorVariable]];
-			breaks = ss.jenks(csvRows.map(function(d) { return +d[colorVariable]; }), 7)
+			breaks = [0, undefined, 100]
 			jenks[colorVariable] = d3.scale.quantile()
 		   	 .domain(breaks)
 		    	.range(colorbrewer.RdPu.mod7)
@@ -1371,7 +1371,7 @@ function getColorScale(row){
 		 }
 	else  if (colorVariable === 'assetmngmt'){
 			var colorDomain = [+_.min(filteredRows,colorVariable)[colorVariable],100];
-			breaks = ss.jenks(csvRows.map(function(d) { return +d[colorVariable]; }),8)
+			breaks = [0, undefined, 100]
 			jenks[colorVariable] = d3.scale.quantile()
 		   	 .domain(breaks)
 		    	.range(colorbrewer.RdPu.mod7)
@@ -1394,7 +1394,8 @@ function getColorScale(row){
 			return jenks[colorVariable](+row[colorVariable]); 
 		}else  if (colorVariable === 'goods_move'){
 			var colorDomain = [+_.min(filteredRows,colorVariable)[colorVariable],100];
-			 breaks = ss.jenks(csvRows.map(function(d) { return +d[colorVariable]; }), 2)
+			// breaks = ss.jenks(csvRows.map(function(d) { return +d[colorVariable]; }), 2)
+			breaks = [0, undefined, 100]
 			jenks[colorVariable] = d3.scale.quantile()
 		    	.domain(breaks)
 		    	.range(colorbrewer.RdPu.mod7)
